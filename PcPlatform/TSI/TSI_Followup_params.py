2f0c8bc32f0de9ee3ea09b0043c9810cb7fc4ee4
Roberto919 <robertosysm@gmail.com>
2020-06-25 12:01:49 -0500

    Creating base file to store all parameters for the project followup

---
+++ PcPlatform/TSI/TSI_Followup_params.py
@@ -0,0 +1,15 @@
+## DATE RELATED TO PROJECT DEVELOPMENT TASKS, DEADLINES AND ADVANCEMENTS
+#### Author: Rob (GH: Roberto919)
+#### Date: 25 June 2020
+
+
+
+
+
+'------------------------------------------------------------------------------------------'
+################
+## Parameters ##
+################
+
+
+## Python libraries
