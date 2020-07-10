1449dc297e555529060b7a19d1c734a8a70bf41c
Roberto919 <robertosysm@gmail.com>
2020-07-10 07:33:57 -0500

    Updates in pharma file

--- Pharma/Pharma_analysis_params.py
+++ Pharma/Pharma_analysis_params.py
@@ -24,18 +24,20 @@ from datetime import *
 
 
 ## Metmorfina pricing
-
 metmorfina_pricing = {
     'Walmart': { #### (850 [mg] - 30 [tablets]) [https://super.walmart.com.mx/medicamentos-de-patente/metformina-medimart-850-mg-30-tabletas/00750222629370]
-        p1: 50,
-        p1_a: 50
+        'p1': 50,
+        'p1_a': 50
     },
     'Farmacias_del_ahorro': { #### (850 [mg] - 60 [tablets]) [https://super.walmart.com.mx/medicamentos-de-patente/metformina-medimart-850-mg-30-tabletas/00750222629370]
-        p1: 158,
-        p1_a: 158/2
+        'p1': 158,
+        'p1_a': 158/2
     },
     'Farmacias_san_pablo': { #### (850 [mg] - 60 [tablets]) [https://www.farmaciasanpablo.com.mx/medicamentos/genericos/m---n---o---p/metformina-850-mg/p/000000000060090039]
-        p1: 104.5,
-        p1_a: 104.5/2
+        'p1': 104.5,
+        'p1_a': 104.5/2
     },
 }
+
+
+## Probability that the patient will buy the drug
