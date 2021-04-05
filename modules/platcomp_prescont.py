e648b74eabc18fe517fea219691ff67f23b529c2
Roberto919 <robertosysm@gmail.com>
2021-04-05 18:33:47 -0500

    relocating files in root directory

---
+++ modules/platcomp_prescont.py
@@ -0,0 +1,60 @@
+# FILE WITH CONTENT FOR THE PRESENTATION
+
+
+
+
+
+"-----------------------------------------------------------------------------------------------------------------------"
+####################
+## Slides content ##
+####################
+
+
+slides_cont = {
+    "intro": {
+        "intro_s1": {
+            "title": "## Objetivo de la presentación",
+            "body_1": "Comparativo entre la plataforma utilizada por Punto Clínico y otras alternativas del mercado",
+            "body_1_img": "https://mecaluxes.cdnwm.com/blog/img/stock-minimo-almacen-reposicion-mercancias.1.6.jpg",
+        },
+        "intro_s2": {
+            "title": "## Motivaciones para desarrollar plataforma",
+            "body_1": "1️⃣  Robustecer las **funcionalidades administrativas**",
+            "body_2": "2️⃣  Automatizar **actividades recurrentes**",
+            "body_3": "️3️⃣  Disponer de **información de negocio** para la toma de decisiones",
+            "body_4": "4️⃣  Integrar **herramientas externas**",
+            "body_5": "5️⃣  Obtener control de los **datos generados**",
+        },
+    },
+    "meto": {
+        "meto_s1": {
+            "title": "## Atributos considerados",
+            "body_1": "Para el comparativo, se tomaron en cuenta 8 atributos relevantes: ",
+            "body_2": "📒  Agenda off-line",
+            "body_3": "🖱  Agenda on-line",
+            "body_4": "🩺  Módulo médico",
+            "body_5": "💻  Teleconsultas",
+            "body_6": "✅  Administración",
+            "body_7": "👛  Finanzas",
+            "body_8": "💁🏼  Atención a cliente",
+            "body_9": "🤯  Otros"
+
+        }
+    },
+    "comp": {
+        "comp_s1": {
+            "title": "## Resultados de las plataformas evaluadas",
+            "body_1": "Resumen del comparativo realizado:"
+        }
+    },
+}
+
+
+
+
+
+"-----------------------------------------------------------------------------------------------------------------------"
+###################
+## END OF MODULE ##
+###################
+"-----------------------------------------------------------------------------------------------------------------------"
